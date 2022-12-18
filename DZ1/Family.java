91b168f740d20f0865b4e2c300d26d539469c9c4
Andzhey123 <beginov.a@gmail.com>
2022-12-18 18:06:41 +0300

    Добавлено решение ДЗ1

---
+++ DZ1/Family.java
@@ -0,0 +1,49 @@
+package DZ1;
+
+import java.util.*;
+
+public class Family {
+	HashMap<String, Person> personMap;
+	String Daddy;
+
+	public Family (String father) {
+		personMap = new HashMap<>();
+		Daddy = father;
+		personMap.put(Daddy, new Person(Daddy));
+	}
+
+	public void birth (String child, String parent) {
+		Person parentObj = personMap.get(parent);
+		Person childObj = new Person(child);
+		parentObj.children.add(childObj);
+		personMap.put(child, childObj);
+	}
+
+	public void death (String name) {
+		personMap.get(name).isDead = true;
+	}
+
+	public List<String> getallChildren() {
+		List<String> succession = new ArrayList<>();
+		Stack<Person> stack = new Stack<>();
+		stack.push(personMap.get(Daddy));
+
+		while ( !stack.empty() ) {
+			Person current = stack.pop();
+			if (!current.isDead) succession.add(current.name);
+			for (int i = current.children.size() - 1; i >= 0; i--) {
+				stack.push(current.children.get(i));
+			}
+		}
+		return succession;
+	}
+
+    public static void main(String[] args) {
+    	Family m = new Family("Иван");
+    	m.birth("Алексей", "Иван");
+    	m.birth("Михаил", "Иван");
+    	m.birth("Виктор", "Михаил");
+    	List<String> list = m.getallChildren();
+    	System.out.println(list.toString());
+    }
+}
---
+++ DZ1/Person.java
@@ -0,0 +1,15 @@
+package DZ1;
+
+import java.util.*;
+
+class Person {
+	public String name;
+	public List<Person> children;
+	public Boolean isDead;
+
+	public Person (String aName) {
+		name = aName;
+		isDead = false;
+		children = new ArrayList<>();
+	}
+}
